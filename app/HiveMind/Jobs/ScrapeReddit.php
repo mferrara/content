21076c08e305ea263f8e156c0522f610a3c3a6bb
mferrara <mferrara@gmail.com>
2014-09-27 22:02:11 -0700

    Change logging type

--- app/HiveMind/Jobs/ScrapeReddit.php
+++ app/HiveMind/Jobs/ScrapeReddit.php
@@ -23,7 +23,7 @@ class ScrapeReddit {
 
 	public function fullScrape($job, $data)
 	{
-		\Log::critical('Job start');
+		\Log::error('Job start');
 		$query 			= \Searchquery::find($data['searchquery_id']);
 		$sort 			= $data['sort_type'];
 		$subs 			= $data['subreddits'];
@@ -37,7 +37,7 @@ class ScrapeReddit {
 
 		$scraper = new Reddit();
 
-		\Log::critical('Before Scraping...');
+		\Log::error('Before Scraping...');
 		if(is_array($time))
 		{
 			foreach($time as $t)
@@ -49,22 +49,22 @@ class ScrapeReddit {
 		{
 			$scraper->Search($query, $page_depth, $search_type, $sort, $time, $subs);
 		}
-		\Log::critical('After Scraping...');
+		\Log::error('After Scraping...');
 
-		\Log::critical('Before Processing');
+		\Log::error('Before Processing');
 
 		// Queue up the processing of the articles
 		// This is after the model is saved because we're triggering the clearing
 		// of this cache by updating of the model
 		\HiveMind\ArticleProcessor::fire($query);
 
-		\Log::critical('After Processing');
+		\Log::error('After Processing');
 
 		$query->scraped = 1;
 		$query->currently_updating = 0;
 		$query->save();
 
-		\Log::critical('Query Saved');
+		\Log::error('Query Saved');
 
 		$job->delete();
 	}
