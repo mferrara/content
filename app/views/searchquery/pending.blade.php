5b24ef0e9e141d3cdf250ea39b701878af6244a6
Mike Ferrara <mferrara@gmail.com>
2015-03-21 04:38:18 -0700

    Add columns to /pending

--- app/views/searchquery/pending.blade.php
+++ app/views/searchquery/pending.blade.php
@@ -4,44 +4,47 @@
 
     {{ link_to('/', 'Home', ['class' => 'btn btn-primary']) }}
 
-    <h1>Pending Searches <small>{{ count($pending_queries) }}</small></h1>
+    <div class="row">
+        <div class="col-md-6 col-xs-12 text-center">
+            <h1>Pending Searches <small>{{ count($pending_queries) }}</small></h1>
 
-    <dl class="dl-horizontal">
-        @foreach($pending_queries as $search)
-            <dt>{{ link_to('search?q='.urlencode($search->name), $search->name) }}</dt>
-            <dd>
-                @if($search->scraped == 0)
-                    <span class="label label-danger">Pending</span>
-                @elseif($search->currently_updating == 1)
-                    <span class="label label-warning">Updating</span>
-                @elseif($search->isStale())
-                    <span class="label label-default">Stale</span>
-                @else
-                    <span class="label label-success">Complete</span>
-                @endif
-            </dd>
-        @endforeach
-    </dl>
+            <dl class="dl-horizontal">
+                @foreach($pending_queries as $search)
+                    <dt>{{ link_to('search?q='.urlencode($search->name), $search->name) }}</dt>
+                    <dd>
+                        @if($search->scraped == 0)
+                            <span class="label label-danger">Pending</span>
+                        @elseif($search->currently_updating == 1)
+                            <span class="label label-warning">Updating</span>
+                        @elseif($search->isStale())
+                            <span class="label label-default">Stale</span>
+                        @else
+                            <span class="label label-success">Complete</span>
+                        @endif
+                    </dd>
+                @endforeach
+            </dl>
+        </div>
+        <div class="col-md-6 col-xs-12 text-center">
+            <h1>Pending Subreddits <small>{{ count($pending_subreddits) }}</small></h1>
 
-    <hr/>
-
-    <h1>Pending Subreddits <small>{{ count($pending_subreddits) }}</small></h1>
-
-    <dl class="dl-horizontal">
-        @foreach($pending_subreddits as $subreddit)
-            <dt>{{ link_to('search?q='.urlencode($subreddit->name), $subreddit->name) }}</dt>
-            <dd>
-                @if($subreddit->scraped == 0)
-                    <span class="label label-danger">Pending</span>
-                @elseif($subreddit->currently_updating == 1)
-                    <span class="label label-warning">Updating</span>
-                @elseif($subreddit->isStale())
-                    <span class="label label-default">Stale</span>
-                @else
-                    <span class="label label-success">Complete</span>
-                @endif
-            </dd>
-        @endforeach
-    </dl>
+            <dl class="dl-horizontal">
+                @foreach($pending_subreddits as $subreddit)
+                    <dt>{{ link_to('search?q='.urlencode($subreddit->name), $subreddit->name) }}</dt>
+                    <dd>
+                        @if($subreddit->scraped == 0)
+                            <span class="label label-danger">Pending</span>
+                        @elseif($subreddit->currently_updating == 1)
+                            <span class="label label-warning">Updating</span>
+                        @elseif($subreddit->isStale())
+                            <span class="label label-default">Stale</span>
+                        @else
+                            <span class="label label-success">Complete</span>
+                        @endif
+                    </dd>
+                @endforeach
+            </dl>
+        </div>
+    </div>
 
 @stop
