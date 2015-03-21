be0f89a9e4a423d5815af7228e85ea64bebc5f2a
Mike Ferrara <mferrara@gmail.com>
2015-03-21 02:07:28 -0700

    Add NSFW flag to posts

--- app/views/partials/articles-table.blade.php
+++ app/views/partials/articles-table.blade.php
@@ -11,19 +11,24 @@
 			<th>Score</th>
 			<th>Subreddit</th>
 			<th>Author</th>
-			<th>Date Posted</th>
+			<th class="text-right" style="width: 120px">Date Posted</th>
 		</tr>
 	</thead>
 	<tbody>
 	@foreach($articles as $article)
 		<tr>
-			<td>{{ link_to('post/'.$article->fullname, $article->title) }}</td>
+			<td>
+                {{ link_to('post/'.$article->fullname, $article->title) }}
+                @if($article->nsfw == 1)
+                    <span class="pull-right label label-warning">NSFW</span>
+                @endif
+            </td>
 			<td>{{ link_to($article->url, '>>', ['target' => '_blank', 'class' => 'btn btn-default btn-xs']) }}</td>
 			<td>{{ $article->content_type }}</td>
-			<td>{{ $article->score }}</td>
+			<td class="text-right">{{ number_format($article->score) }}</td>
 			<td>{{ link_to('sub/'.$article->subreddit->name, $article->subreddit->name) }}</td>
 			<td>{{ link_to('author/'.$article->author->name, $article->author->name) }}</td>
-			<td>{{ \Carbon\Carbon::createFromTimeStamp($article->created)->toFormattedDateString() }}</td>
+			<td class="text-right">{{ \Carbon\Carbon::createFromTimeStamp($article->created)->toFormattedDateString() }}</td>
 		</tr>
 	@endforeach
 	</tbody>
